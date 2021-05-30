e8247a7ea91783e561178e951ee76b2c39b970c4
Lukas <lukyn.odehnal333@gmail.com>
2021-05-30 12:37:12 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React,{useState} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import './App.css'
 
 function App() {
-  const [input,setInput] = useState('')
-  const [l,setL] = useState('1vw')
+  const [input,setInput] = useState('');
+  const [l,setL] = useState('1vw');
+  const timesRendered = useRef(0);
+
+  useEffect(()=>{
+    timesRendered.current = timesRendered.current+1
+  })
   
   function Change(e){
     setInput(e.target.value);
     setL(`${input.split("").length}px`);
-    console.log(l);
   }
   
   return (
     <div className="App">
+      <h2>Rerendered {timesRendered.current} times</h2>
       <h1 style={{fontSize : l}}>{input}</h1>
       <textarea type="text" value={input} onChange={e => Change(e)}/>
+      
     </div>
   );
 }
